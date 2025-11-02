79c4197c2df8cb00c5ac7a392452e8a32a37fc42
Jefferson <jeffersoonpaez@gmail.com>
2025-11-01 23:30:16 -0400

    started this proyect

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+try {
+  $pdo = new PDO('mysql:host=localhost;dbname=employees;charset=utf8', 'root', '');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  die("Error conexión: " . $e->getMessage());
+}
+?>
