8729d06b294fdd48c706503070666ad3c9e5b728
Jefferson <jeffersoonpaez@gmail.com>
2025-11-13 23:59:39 -0400

    exportada la base de datos actualizada

--- plataformaCitasMedicas/config/appointment_platform.sql
+++ plataformaCitasMedicas/config/appointment_platform.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 08-11-2025 a las 03:51:29
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: Nov 14, 2025 at 03:57 AM
+-- Server version: 8.4.3
+-- PHP Version: 8.3.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,22 +18,22 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `appointment_platform`
+-- Database: `appointment_platform`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `cities`
+-- Table structure for table `cities`
 --
 
 CREATE TABLE `cities` (
-  `id` int(11) NOT NULL,
-  `name` varchar(80) NOT NULL
+  `id` int NOT NULL,
+  `name` varchar(80) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `cities`
+-- Dumping data for table `cities`
 --
 
 INSERT INTO `cities` (`id`, `name`) VALUES
@@ -43,16 +43,27 @@ INSERT INTO `cities` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `typeusers`
+-- Table structure for table `specialty`
+--
+
+CREATE TABLE `specialty` (
+  `id` int NOT NULL,
+  `nombre` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `typeusers`
 --
 
 CREATE TABLE `typeusers` (
-  `id` int(11) NOT NULL,
-  `name` varchar(80) NOT NULL
+  `id` int NOT NULL,
+  `name` varchar(80) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `typeusers`
+-- Dumping data for table `typeusers`
 --
 
 INSERT INTO `typeusers` (`id`, `name`) VALUES
@@ -63,22 +74,22 @@ INSERT INTO `typeusers` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(80) DEFAULT NULL,
-  `password` varchar(80) DEFAULT NULL,
-  `name` varchar(80) DEFAULT NULL,
-  `lastname` varchar(80) DEFAULT NULL,
+  `id` int NOT NULL,
+  `username` varchar(80) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password` varchar(80) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `name` varchar(80) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `lastname` varchar(80) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `birthdate` date DEFAULT NULL,
-  `idCity` int(11) DEFAULT NULL,
-  `idTypeUser` int(11) DEFAULT NULL
+  `idCity` int DEFAULT NULL,
+  `idTypeUser` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `name`, `lastname`, `birthdate`, `idCity`, `idTypeUser`) VALUES
@@ -86,23 +97,30 @@ INSERT INTO `users` (`id`, `username`, `password`, `name`, `lastname`, `birthdat
 (16, 'Alejandro', '220601', 'Martin', 'aguzman', '2013-02-07', 1, 2);
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `cities`
+-- Indexes for table `cities`
 --
 ALTER TABLE `cities`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `typeusers`
+-- Indexes for table `specialty`
+--
+ALTER TABLE `specialty`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indexes for table `typeusers`
 --
 ALTER TABLE `typeusers`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
@@ -111,33 +129,39 @@ ALTER TABLE `users`
   ADD KEY `idTypeUser` (`idTypeUser`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `cities`
+-- AUTO_INCREMENT for table `cities`
 --
 ALTER TABLE `cities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `typeusers`
+-- AUTO_INCREMENT for table `specialty`
+--
+ALTER TABLE `specialty`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `typeusers`
 --
 ALTER TABLE `typeusers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de la tabla `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `users`
+-- Constraints for table `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`idCity`) REFERENCES `cities` (`id`),
