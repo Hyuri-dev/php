b5ed7ae874365dc512e3417f62bf8fd913b57af7
Hyuri.dev <jeffersoonpaez@gmail.com>
2025-11-14 17:03:26 -0400

    Añadido comentario en el database. Empezado el login y su funcion de autenticación pendiente por terminar

--- plataformaCitasMedicas/config/database.php
+++ plataformaCitasMedicas/config/database.php
@@ -1,5 +1,7 @@
 <?php
 try {
+
+  // Cambiar la contraseña a vacio si se tiene el usuario del mariadb o mysql con contraseña
   $conn = new PDO('mysql:host=localhost;dbname=appointment_platform;charset=utf8', 'root', 'aquiles');
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
