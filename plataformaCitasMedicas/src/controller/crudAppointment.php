30a951da4371285d48a776a4b6f87346a57e0152
Hyuri.dev <jeffersoonpaez@gmail.com>
2025-12-11 15:39:07 -0400

    Agregar controlador para la gestión de citas y respuesta en formato JSON

---
+++ plataformaCitasMedicas/src/controller/crudAppointment.php
@@ -0,0 +1,32 @@
+<?php 
+require_once '../php/plataformaCitasMedicas/config/database.php';
+
+header('Content-Type: application/json'); 
+
+
+try {
+  $idUser = $_POST['idUser'];
+  $idDoctor = $_POST['idDoctor'];
+  $idSpecialty = $_POST['idSpecialty'];
+  $dateAppointment = $_POST['dateAppointment'];
+  $idStatus = 3;
+
+  $sql = "INSERT INTO appointment (idUser , idDoctor, idSpecialty, dateAppointment, idStatus)
+  VALUES (:user, :doctor, :specialty, :date, :status)";
+
+  $stmt = $conn -> prepare($sql);
+  $stmt->execute([
+    ':user'=>$idUser,
+    ':doctor'=>$idDoctor,
+    ':specialty'=>$dateAppointment,
+    ':date'=>$dateAppointment,
+    ':status'=>$idStatus
+  ]);
+
+  echo json_encode(['success'=> true, 'message'=>'Cita agendada correctamente']);
+} catch (Exception $e){
+  echo json_encode(['success'=> false, 'message' => 'Error'.$e->getMessage()]);
+}
+
+
+?>
